158012798a216e25ca89acc4ad408f220937106c
Junior Sousa <jrsousadev@gmail.com>
2022-11-24 01:25:15 -0300

    create chatContext

---
+++ src/contexts/ChatContext.tsx
@@ -0,0 +1,141 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useCallback,
+} from "react";
+import { Message } from "../domain/Message";
+import { useUser } from "./UserContext";
+import { ContactFormated, mapFormatContactArray } from "../domain/Contact";
+import { getAllChatsByUser } from "../services/chat-api";
+import { BASE_SOCKET_BACKEND } from "../environments/values";
+
+import io from "socket.io-client";
+
+export type Friend = {
+  chatId: string;
+  name: string;
+  image: string;
+};
+
+type ChatContextData = {
+  friend: Friend | null;
+  contacts: ContactFormated[];
+  allMessages: Message[];
+
+  handleSelectFriend: (selectFriend: Friend) => void;
+  handleSetNewMessages: (newMessage: AddNewMessage) => void;
+  handleFilterContacts: (value: string) => void;
+};
+
+type ChatProviderProps = {
+  children: React.ReactNode;
+};
+
+interface AddNewMessage {
+  issuer: string;
+  text: string;
+}
+
+const ChatContext = createContext({} as ChatContextData);
+export const socket = io(String(BASE_SOCKET_BACKEND));
+
+export const ChatProvider = ({ children }: ChatProviderProps) => {
+  const { user } = useUser();
+
+  const [friend, setFriend] = useState<Friend | null>(null);
+  const [allMessages, setAllMessages] = useState<Message[]>([]);
+
+  const [contactsMemory, setContactsMemory] = useState<ContactFormated[]>([]);
+  const [contacts, setContacts] = useState<ContactFormated[]>([]);
+
+  socket.on("message", (newMessage) => {
+    const newArray = [...allMessages];
+    newArray.unshift(newMessage);
+    setAllMessages(newArray);
+  });
+
+  useEffect(() => {
+    if (typeof friend?.chatId !== "undefined") {
+      socket.emit(
+        "select_chat",
+        {
+          chatId: friend?.chatId,
+        },
+        (response: Message[]) => setAllMessages(response)
+      );
+    }
+  }, [friend]);
+
+  useEffect(() => {
+    (async () => {
+      if (user?.id) {
+        const result = await getAllChatsByUser(user.id);
+        const resultFormated = mapFormatContactArray(result);
+
+        setContacts(resultFormated);
+        setContactsMemory(resultFormated);
+      }
+    })();
+  }, [user]);
+
+  const handleSelectFriend = useCallback((data: Friend) => {
+    setFriend(data);
+  }, []);
+
+  const handleSetNewMessages = useCallback(
+    ({ issuer, text }: AddNewMessage) => {
+      if (typeof friend?.chatId !== "undefined") {
+        const newMessage = {
+          issuer,
+          text,
+          chatId: friend?.chatId,
+        };
+
+        socket.emit("message", {
+          message: newMessage,
+        });
+      }
+    },
+    [friend?.chatId]
+  );
+
+  const handleFilterContacts = useCallback(
+    (value: string) => {
+      if (value.length <= 0) return setContacts(contactsMemory);
+
+      const filteredContacts = contacts.filter(({ userContact }) => {
+        const startsWith = userContact?.name
+          .toLowerCase()
+          .startsWith(value.toLowerCase());
+        const includes = userContact?.name
+          .toLowerCase()
+          .includes(value.toLowerCase());
+
+        if (startsWith) return userContact;
+        if (includes) return userContact;
+      });
+
+      setContacts(filteredContacts);
+    },
+    [contacts, contactsMemory]
+  );
+
+  return (
+    <ChatContext.Provider
+      value={{
+        contacts,
+        friend,
+        allMessages,
+        handleSelectFriend,
+        handleSetNewMessages,
+        handleFilterContacts,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const useChat = () => useContext(ChatContext);
